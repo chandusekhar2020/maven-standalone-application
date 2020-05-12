ed6973693e5cf64eb079250212022286060f977b
chandusekhar2020 <60094053+chandusekhar2020@users.noreply.github.com>
2020-05-12 07:46:06 +0530

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -14,7 +14,7 @@ public class HelloWorld {
 	}
 
 	private static Date getLocalCurrentDate() {
-		String m = "Mithun Technologies";
+		String m = "Welcome to the DevOps";
 		return new Date();
 
 	}
